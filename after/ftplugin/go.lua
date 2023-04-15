868edbd874e7ca09b91b605478229d2ae202c17d
ui-HookeyChiang <hookey.chiang@ui.com>
2023-04-15 14:31:08 +0800

    docs: comment tabexpand on go

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -1,4 +1,6 @@
 vim.opt_local.commentstring = '//%s'
+-- expandtab with false will cause go format error
+-- but true will cause auto complete not working well
 vim.opt_local.expandtab = false
 vim.opt_local.shiftwidth = 4
 vim.opt_local.softtabstop = 4
