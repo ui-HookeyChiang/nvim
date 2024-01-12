b8109f797bcb404544dd77f1b6d167c3fdffd998
ui-HookeyChiang <hookey.chiang@ui.com>
2024-01-12 04:02:43 +0000

    ui: indent char

--- lua/modules/ui/package.lua
+++ lua/modules/ui/package.lua
@@ -24,9 +24,8 @@ packadd({
   'nvimdev/indentmini.nvim',
   event = { 'BufEnter' },
   config = function()
-    vim.opt.listchars:append({ tab = '  ' })
     require('indentmini').setup({
-      only_current = true,
+      char = '»',
     })
   end,
 })
