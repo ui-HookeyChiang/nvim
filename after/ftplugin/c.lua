4e42d0299adc8d65c5d04cef96b524821b54b402
ui-HookeyChiang <hookey.chiang@ui.com>
2024-05-27 07:27:11 +0000

    Set textwidth and colorcolumn to 80 in C, C++, and proto ftplugins

--- after/ftplugin/c.lua
+++ after/ftplugin/c.lua
@@ -5,8 +5,7 @@ local v = name and name:find('neovim') and 2 or 4
 vim.opt_local.commentstring = '//%s'
 vim.opt_local.autoindent = true
 vim.opt_local.smartindent = true
-vim.opt_local.shiftwidth = v
-vim.opt_local.softtabstop = v
-vim.opt_local.tabstop = v
+vim.opt_local.shiftwidth = v -- 8 kernel
+vim.opt_local.softtabstop = v -- 4
+vim.opt_local.tabstop = v -- 8 kernel
 vim.opt_local.expandtab = true --false?
-
--- after/ftplugin/proto.lua
+++ after/ftplugin/proto.lua
@@ -1,3 +1,5 @@
 vim.opt_local.cindent = true
 vim.opt_local.expandtab = true
 vim.opt_local.shiftwidth = 2
+vim.opt.textwidth = 80
+vim.opt.colorcolumn = '80'
