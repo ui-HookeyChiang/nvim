7ecde9cd74c7aa450f533dce7328ba717914c557
ui-HookeyChiang <hookey.chiang@ui.com>
2024-10-31 10:25:09 +0000

    tools: no auto format c

--- lua/modules/tools/config.lua
+++ lua/modules/tools/config.lua
@@ -10,12 +10,12 @@ end
 
 function config.guard()
   local ft = require('guard.filetype')
-  ft('c,cpp'):fmt({
-    cmd = 'clang-format',
-    stdin = true,
-    ignore_patterns = { 'neovim', 'vim' },
-  })
-
+  -- ft('c,cpp'):fmt({
+  --   cmd = 'clang-format',
+  --   stdin = true,
+  --   ignore_patterns = { 'neovim', 'vim' },
+  -- })
+  --
   ft('lua'):fmt({
     cmd = 'stylua',
     args = { '-' },
