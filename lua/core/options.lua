e8f00771fa305146276c65c99a028c6a8eb80e6e
ui-HookeyChiang <hookey.chiang@ui.com>
2025-03-22 23:09:07 +0800

    vim: no pbcopy

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -87,3 +87,17 @@ function _G.show_stc()
 end
 
 opt.stc = '%!v:lua.show_stc()'
+-- if vim.uv.os_uname().sysname == 'Darwin' then
+--   vim.g.clipboard = {
+--     name = 'macOS-clipboard',
+--     copy = {
+--       ['+'] = 'pbcopy',
+--       ['*'] = 'pbcopy',
+--     },
+--     paste = {
+--       ['+'] = 'pbpaste',
+--       ['*'] = 'pbpaste',
+--     },
+--     cache_enabled = 0,
+--   }
+-- end
