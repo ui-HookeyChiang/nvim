7ceed14c3199e0adc3d08c6488fcbf14f83cc27f
ui-HookeyChiang <hookey.chiang@ui.com>
2026-06-16 12:30:30 +0000

    fix(clipboard): move osc52 setup to UIEnter in events.lua

--- init.lua
+++ init.lua
@@ -328,22 +328,4 @@ P:add({
       require('gomove').setup({})
     end),
   })
-  :add('ojroques/nvim-osc52', {
-    load = on_event('UIEnter', 'nvim-osc52', function()
-      local osc52 = require('osc52')
-      osc52.setup({ max_length = 0, silent = true, trim = false, tmux_passthrough = true })
-      local function copy(lines, _)
-        osc52.copy(table.concat(lines, '\n'))
-      end
-      local function paste()
-        return { vim.fn.split(vim.fn.getreg('"'), '\n'), vim.fn.getregtype('"') }
-      end
-      vim.g.clipboard = {
-        name = 'osc52',
-        copy = { ['+'] = copy, ['*'] = copy },
-        paste = { ['+'] = paste, ['*'] = paste },
-        cache_enabled = false,
-      }
-      vim.o.clipboard = 'unnamedplus'
-    end),
-  })
+  :add('ojroques/nvim-osc52', { load = false })
